2d587f8283738f11133ee2874e23f8500d77f93c
namnnnovaon <namnn@novaon.vn>
2014-12-12 08:16:07 +0700

    commit 1

---
+++ TestAjax/TestAjax/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string checkEmail(String email)
+        {
+            string val = "";
+            if (email.Contains("@"))
+            {
+                if (email.Contains(".com"))
+                {
+                    val = "correct";
+                }
+            }
+            else
+            {
+                val= "incorrect";
+            }
+            return val;
+        }
+    }
+}
